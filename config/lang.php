eb83fc078400e98eabeafc4852c19e5afd43462d
Web Master <danielchavira1@gmail.com>
2015-07-30 14:30:54 -0500

    English lang file finished

--- config/lang.php
+++ config/lang.php
@@ -1,20 +1,43 @@
 <?php
 
 return [
-	'page-title' => 'pfSense csv to xml',
-	'file-config-link' => 'Create XML from a .csv file',
-	'index' => [
-		'h1' => 'Upload csv file',
-		'note' => 'File must have the following fields in the exact order:',
-		'fields' => [
-			'Username',
-			'Password',
-			'Description'
-		],
-		'file-name' => '.csv File',
-		'submit' => 'Upload file',
+	'general' => [
+		'page-title' => 'pfSense csv to xml',
 	],
-	'' => '',
-	'' => '',
-	'' => '',
+	'link-text' => [
+		'file-config' => 'Create XML from a .csv file',
+		'file-upload' => 'Upload new file'
+	],
+	'messages' => [
+		'upload' => [
+			'success' => 'File uploaded!',
+			'failure' => 'File upload failed, please try again'
+		]
+	],
+	'pages' => [
+		'index' => [
+			'h1' => 'Upload csv file',
+			'note' => 'File must have the following fields in the exact order:',
+			'fields' => [
+				'Username',
+				'Password',
+				'Description'
+			],
+			'form' => [
+				'file' => '.csv File',
+				'submit' => 'Upload file',
+			]
+		],
+		'options' => [
+			'h1' => 'Output settings',
+			'file' => '.csv file',
+			'uid' => 'starting id',
+			'format' => 'Output format',
+			'form' => [
+				'default' => 'Select one',
+				'html-output' => 'View as HTML',
+				'source-output' => 'View from source',
+			]
+		]
+	]
 ];
