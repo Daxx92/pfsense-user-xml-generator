8ef03e3d7494bec38b5bdb18cacc52b2bb8930d1
Web Master <danielchavira1@gmail.com>
2015-07-30 14:23:18 -0500

    Starting language file config

---
+++ config/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+	'page-title' => 'pfSense csv to xml',
+	'file-config-link' => 'Create XML from a .csv file',
+	'index' => [
+		'h1' => 'Upload csv file',
+		'note' => 'File must have the following fields in the exact order:',
+		'fields' => [
+			'Username',
+			'Password',
+			'Description'
+		],
+		'file-name' => '.csv File',
+		'submit' => 'Upload file',
+	],
+	'' => '',
+	'' => '',
+	'' => '',
+];
