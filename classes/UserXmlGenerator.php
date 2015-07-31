7fed7576bc76c2127bcb712ff9cf112d6a3a2fc4
Web Master <danielchavira1@gmail.com>
2015-07-31 14:06:50 -0500

    Fixed missing semicolons and array indexes

--- classes/UserXmlGenerator.php
+++ classes/UserXmlGenerator.php
@@ -1,28 +1,28 @@
 <?php
 
-class UserXMLGenerator.php{
+class UserXMLGenerator{
 
 	private $tags = [':-{password}-:', ':-{md5-hash}-:', ':-{nt-hash}-:', ':-{name}-:', ':-{descr}-:', ':-{uid}-:'];
 	private $xml =    '		<user>' . PHP_EOL
-					. '			<scope>user</scope>' . PHP_EOL;
-					. '			<password>:-{password}-:</password>' . PHP_EOL;
-					. '			<md5-hash>:-{md5-hash}-:</md5-hash>' . PHP_EOL;
-					. '			<nt-hash>:-{nt-hash}-:</nt-hash>' . PHP_EOL;
-					. '			<name>:-{name}-:</name>' . PHP_EOL;
-					. '			<descr><![CDATA[:-{descr}-:]]></descr>' . PHP_EOL;
-					. '			<expires/>' . PHP_EOL;
-					. '			<authorizedkeys/>' . PHP_EOL;
-					. '			<ipsecpsk/>' . PHP_EOL;
-					. '			<uid>:-{uid}-:</uid>' . PHP_EOL;
+					. '			<scope>user</scope>' . PHP_EOL
+					. '			<password>:-{password}-:</password>' . PHP_EOL
+					. '			<md5-hash>:-{md5-hash}-:</md5-hash>' . PHP_EOL
+					. '			<nt-hash>:-{nt-hash}-:</nt-hash>' . PHP_EOL
+					. '			<name>:-{name}-:</name>' . PHP_EOL
+					. '			<descr><![CDATA[:-{descr}-:]]></descr>' . PHP_EOL
+					. '			<expires/>' . PHP_EOL
+					. '			<authorizedkeys/>' . PHP_EOL
+					. '			<ipsecpsk/>' . PHP_EOL
+					. '			<uid>:-{uid}-:</uid>' . PHP_EOL
 					. '		</user>' . PHP_EOL;
 
 	private $usernamePos = 0;
 	private $passwordPos = 1;
-	private $descriptionPos = 3;
+	private $descriptionPos = 2;
 
 	protected $pfSenseEncrypt;
 
-	public __constructor( pfSenseEncrypt $pfSenseEncrypt ){
+	public function __construct( pfSenseEncrypt $pfSenseEncrypt ){
 		$this->pfSenseEncrypt = $pfSenseEncrypt;
 	}
 
@@ -32,7 +32,7 @@ class UserXMLGenerator.php{
 		$xml = [];
 
 		//open file
-		$file = fopen( $filename, 'r')
+		$file = fopen( $filename, 'r');
 
 		if( $file ){
 			//read line as csv
@@ -56,20 +56,21 @@ class UserXMLGenerator.php{
 	/* Creates the XML of a user with the given data and uid*/
 	private function userXML( $uid, $data ){
 		//Get pfSense style encrypted passwords
+
 		$tmpPasswords = $this->pfSenseEncrypt->encryptPassword( $data[ $this->passwordPos ] );
 
 		//Make an array of replacements that match tags positions
 		$userData = [
-			$passwords[ $this->pfSenseEncrypt->PASSWORD ],
-			$passwords[ $this->pfSenseEncrypt->MD5 ],
-			$passwords[ $this->pfSenseEncrypt->NT_HASH ],
-			$data[$this->UsernamePos],
-			$data[$this->DescriptionPos],
+			$tmpPasswords[ pfSenseEncrypt::PASSWORD ],
+			$tmpPasswords[ pfSenseEncrypt::MD5 ],
+			$tmpPasswords [ pfSenseEncrypt::NT_HASH ],
+			$data[$this->usernamePos],
+			$data[$this->descriptionPos],
 			$uid
 		];
 
 		//Return the XML with everything replaced
-		return str_ireplace($tags, $user, $userXML);
+		return str_ireplace($this->tags, $userData, $this->xml);
 
 	}
 
